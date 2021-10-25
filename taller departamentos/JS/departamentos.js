ab42d5d787e02a57b327014dd52a88a374697a9c
Yanelys01 <91100469+Yanelys01@users.noreply.github.com>
2021-10-24 20:47:44 -0500

    Add files via upload

---
+++ taller departamentos/JS/departamentos.js
@@ -0,0 +1,111 @@
+const departamento = () => {
+    let departamento = prompt(
+        "Ingresa un Departamento para conocer su Capital"
+    );
+    switch (departamento) {
+        case "Amazonas":
+            alert("Capital: Leticia")
+            break;
+        case "Antioquía":
+            alert("Capital: Medellin")
+            break;
+        case "Arauca":
+            alert("Capital: Arauca")
+            break;
+        case "Atlántico":
+            alert("Capital: Barranquilla")
+            break;
+        case "Bolívar":
+            alert("Capital: Cartagena de Indias")
+            break;
+        case "Boyacá":
+            alert("Capital: Tunja")
+            break;
+        case "Caldas":
+            alert("Capital: Manizales")
+            break;
+        case "Caquetá":
+            alert("Capital: Florencia")
+            break;
+        case "Casanare":
+            alert("Capital: Yopal")
+            break;
+        case "Cauca":
+            alert("Capital: Popayán")
+            break;
+        case "Casar":
+            alert("Capital: Valledupar")
+            break;
+        case "Chocó":
+            alert("Capital: Quibdó")
+            break;
+        case "Córdoba":
+            alert("Capital: Montería")
+            break;
+        case "Cundinamarca":
+            alert("Capital: Bogotá")
+            break;
+        case "Guainía":
+            alert("Capital: Inírida")
+            break;
+        case "Guaviare":
+            alert("Capital: San José del Guaviare")
+            break;
+        case "Huila":
+            alert("Capital: Neiva")
+            break;
+        case "La Guajira":
+            alert("Capital: Riohacha")
+            break;
+        case "Magdalena":
+            alert("Capital: Santa Marta")
+            break;
+        case "Meta":
+            alert("Capital: Villavicencio")
+            break;
+        case "Nariño":
+            alert("Capital: San Juan de Pasto")
+            break;
+        case "Norte de Santander":
+            alert("Capital: SAn Jose de Cúcuta")
+            break;
+        case "Putumayo":
+            alert("Capital: Mocoa")
+            break;
+        case "Quindío":
+            alert("Capital: Armenia")
+            break;
+        case "Risaralda":
+            alert("Capital: Pereira")
+            break;
+        case "San Andrés y Providencia":
+            alert("Capital: San Andrés")
+            break;
+        case "Santander":
+            alert("Capital: Bucaramanga")
+            break;
+        case "Sucre":
+            alert("Capital: Sincelejo")
+            break;
+        case "Tolima":
+            alert("Capital: Ibagué")
+            break;
+        case "Valle del Cauca":
+            alert("Capital: Cali")
+            break;
+        case "Vaupés":
+            alert("Capital: Mitú")
+            break;
+        case "Vichada":
+            alert("Capital: Puerto Carreño")
+            break;
+
+
+        default:
+            alert("Escribe un departamento");
+            break;
+
+    }
+
+
+}
